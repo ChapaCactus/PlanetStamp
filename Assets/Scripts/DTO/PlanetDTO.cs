104041f234c438af11ba72c0f318fdc6c96664cc
Chapa.C(Takuya) <chapatown@gmail.com>
2017-11-30 15:29:44 +0900

    Planetのデータクラス作成とセットアップ

---
+++ Assets/Scripts/DTO/PlanetDTO.cs
@@ -0,0 +1,11 @@
+﻿using PlanetStamp;
+
+public class PlanetDTO
+{
+	private PlanetVO m_vo;
+
+	public void SetVO(PlanetVO vo)
+	{
+		m_vo = vo;
+	}
+}
--- Assets/Scripts/Planet/Planet.cs
+++ Assets/Scripts/Planet/Planet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -7,5 +7,29 @@ namespace PlanetStamp
 {
 	public class Planet : MonoBehaviour
 	{
+		private PlanetDTO m_planetDTO;
+
+		private static readonly string PREFAB_PATH = "";
+
+		public static void Create(int id, Transform parent, Action<Planet> callback)
+		{
+			var prefab = Resources.Load(PREFAB_PATH) as GameObject;
+			var go = Instantiate(prefab, parent);
+
+			var planet = go.GetComponent<Planet>();
+
+			var dto = new PlanetDTO();
+			var vo = new PlanetVO();// ほんとはMasterからロードする
+			dto.SetVO(vo);
+
+			planet.Setup(dto);
+
+			callback(planet);
+		}
+
+		private void Setup(PlanetDTO dto)
+		{
+			m_planetDTO = dto;
+		}
 	}
 }
