69b92b82e36327a5697540e6d2bdd0e2fe7ee972
Chapa.C(Takuya) <chapatown@gmail.com>
2017-11-30 17:47:13 +0900

    Master修正

--- Assets/Scripts/ScriptableObject/PlanetMaster.cs
+++ Assets/Scripts/ScriptableObject/PlanetMaster.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 
 using PlanetStamp;
 
@@ -7,6 +9,24 @@ namespace PlanetStamp
 	[CreateAssetMenu]
 	public class PlanetMaster : ScriptableObject
 	{
-		public PlanetVO[] rows;
+		[SerializeField]
+		private PlanetVO[] m_rows;
+
+		public PlanetVO[] Rows { get { return m_rows; } }
+
+		public void GetRow(string id, Action<PlanetVO> callback)
+		{
+			foreach(var row in Rows)
+			{
+				if(row.ID == id)
+				{
+					callback(row);
+					return;
+				}
+			}
+
+			// 見つからなかった場合
+			callback(null);
+		}
 	}
 }
--- Assets/Scripts/VO/PlanetVO.cs
+++ Assets/Scripts/VO/PlanetVO.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
+using UnityEngine;
+
 namespace PlanetStamp
 {
 	[Serializable]
 	public class PlanetVO
 	{
-		public string name;
+		[SerializeField]
+		private string m_id;
 
-		public Enums.PlanetType type;
+		[SerializeField]
+		private string m_name;
+
+		[SerializeField]
+		private Enums.PlanetType m_type;
+
+		[SerializeField]
+		private string m_prefabPath;
+
+		public string ID { get { return m_id; } }
+
+		public string Name { get { return m_name; } }
+
+		public Enums.PlanetType Type { get { return m_type; } }
+
+		/// <summary>
+		/// Resources以下のパス
+		/// </summary>
+		public string PrefabPath { get { return m_prefabPath; } }
 	}
 }
