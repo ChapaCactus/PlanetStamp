452e13c6d16b8ace329d8dbf03919b3c8389c2aa
Chapa.C(Takuya) <chapatown@gmail.com>
2017-12-03 19:46:18 +0900

    着地時のパーティクル作成

--- Assets/Scripts/Planet/PlanetView.cs
+++ Assets/Scripts/Planet/PlanetView.cs
@@ -8,9 +8,21 @@ namespace PlanetStamp
 		[SerializeField]
 		private SpriteRenderer m_mainRenderer;
 
+		[SerializeField]
+		private ParticleSystem m_landingEffect;
+
 		private void Awake()
 		{
 			Assert.IsNotNull(m_mainRenderer);
+			Assert.IsNotNull(m_landingEffect);
+		}
+
+		public void PlayLandingEffect(Vector2 landingPos)
+		{
+			if (m_landingEffect == null) return;
+
+			m_landingEffect.transform.localPosition = landingPos;
+			m_landingEffect.Play();
 		}
 	}
 }
